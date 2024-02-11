0cc48b3b6f091f5906e0cb295f189672f5119242
Lizzy-2025 <159677331+Lizzy-2025@users.noreply.github.com>
2024-02-11 12:49:26 -0500

    Update FarmCosts.java

--- FarmCosts.java
+++ FarmCosts.java
@@ -1,4 +1,45 @@
-public class FarmCosts
+public class FeedPrice
 {
+    /**
+        Holds prices of Animal feed
+    */
+    double priceAlpaca;
+    double priceGoat;
+    double priceChicken;
+    
+    /**
+        Constructor
+        @param pA The price of Alpaca feed
+        @param pG The price of Goat feed
+        @param pC The price of Chicken feed
+    */
+    public FarmCosts(double pA, double pG, double pC)
+    {
+        priceAlpaca=pA;
+        priceGoat=pG;
+        priceChicken=pC;
+    }
+    
+    /**
+        Constructor
+    */
+    public FarmCosts()
+    {
+        priceAlpaca=0;
+        priceGoat=0;
+        priceChicken=0;
+    }
 
+    public double setAlpacaPrice(double pA)
+    {
+        if (pA > 0)
+            priceAlpaca = pA;
+        else
+            System.out.println("The price can't be below $0.00.");
+    }
+
+    public getAlpacaPrice()
+    {
+        return priceAlpaca;
+    }
 }
